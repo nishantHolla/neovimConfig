07f290774e9793ca23ae8094e55e8d0d389a3d25
nishantHolla <nishantholla19@gmail.com>
2023-08-03 10:05:26 +0530

    Add snippets keymaps

--- lua/modules/plugins/nvimCmp/init.lua
+++ lua/modules/plugins/nvimCmp/init.lua
@@ -2,6 +2,16 @@
 
 local cmp = require('cmp')
 
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -16,6 +26,22 @@ cmp.setup({
 		['<a-k>'] = cmp.mapping.select_prev_item(),
 		['<a-e>'] = cmp.mapping.abort(),
 		['<a-;>'] = cmp.mapping.confirm({ select = true }),
+		['<a-.>'] = cmp.mapping(function(fallback)
+			if vim.fn["vsnip#available"](1) == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			else
+				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+			end
+		end, { "i", "s" }),
+
+		['<a-,>'] = cmp.mapping(function(fallback)
+			if vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
 	}),
 
 	sources = cmp.config.sources(
