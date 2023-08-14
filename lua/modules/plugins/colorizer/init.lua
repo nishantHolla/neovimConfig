1805874c356a896bfbe6e89980954239af60d47b
nishantHolla <nishantholla19@gmail.com>
2023-08-14 20:34:40 +0530

    Add colorizer plugin

---
+++ lua/modules/plugins/colorizer/init.lua
@@ -0,0 +1,7 @@
+-- colorizer plugin
+
+-- This option is set by options module also, but this plugin requires the option to be set
+-- before initializing
+vim.opt.termguicolors = true
+
+require('colorizer').setup()
